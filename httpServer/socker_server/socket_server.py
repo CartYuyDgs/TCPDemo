2fca84fbd51596b90b96b5b78f088c0bad91d274
于洋 <yy_dgs@126.com>
2020-06-19 11:15:50 +0800

    add 注释

--- httpServer/socker_server/socket_server.py
+++ httpServer/socker_server/socket_server.py
@@ -3,13 +3,14 @@ import socket
 
 class TcpServer():
 
+    #初始化
     def __init__(self, server_address, handler_class):
         self.ServerAddress = server_address
         self.HandlerClass = handler_class
         self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.is_down  = False
 
-
+    # 启动
     def server_forever(self):
         self.Socket.bind(self.ServerAddress)
         self.Socket.listen(10)
@@ -23,23 +24,23 @@ class TcpServer():
             finally:
                 self.close_request(request)
 
-
+    # 获取请求
     def get_request(self):
         return self.Socket.accept()
 
 
-
+    # 处理请求
     def process_request(self, request, client_address):
         handler = self.HandlerClass(request,client_address)
         handler.handler()
         pass
 
-
+    # 关闭请求
     def close_request(self, request):
         request.shutdown()
         request.close()
 
-
+    # 关闭server
     def shutdown(self):
         self.is_down = True
         pass
