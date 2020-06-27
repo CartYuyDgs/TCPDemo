adbb21d5238ff1c5acc856f92d3648f442b66f99
于洋 <yy_dgs@126.com>
2020-06-27 12:22:54 +0800

    http POST方法实现

--- httpServer/handler/simple_http_handler.py
+++ httpServer/handler/simple_http_handler.py
@@ -1,6 +1,7 @@
 # -*- encoding=utf-8 -*-
 
 from urllib import parse
+import json
 from httpServer.handler.base_httphandler import BaseHttpRequestHandler
 import os
 RESOURCES_PATH = os.path.join(os.path.abspath(os.path.dirname(__name__)),"../resources")
@@ -41,4 +42,17 @@ class SimpleHTTPRequestHandler(BaseHttpRequestHandler):
         else:
             return False, resource_path
     def do_POST(self):
-        pass
+        body = json.loads(self.body)
+
+        username = body['username']
+        password = body['password']
+
+        if username == 'dongdongqiang' and password == '123456':
+            response = {'message':'success', 'code': 0}
+        else:
+            response = {'message': 'false', 'code': -1}
+        response = json.dumps(response)
+        self.write_response(200)
+        self.write_header('Content-Length', len(response))
+        self.end_header()
+        self.write_content(response)
